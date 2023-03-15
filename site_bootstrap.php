7083bde30a88dc85355364904da40a29a9ddbecd
Lost4295 <turin-ylan@outlook.fr>
2023-03-15 14:06:36 +0100

    add bootstrap css

--- site_bootstrap.php
+++ site_bootstrap.php
@@ -1,1 +1,12 @@
-:D
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap demo</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    </head>
+    <body>
+        <h1>Hello, world!</h1>
+    </body>
+</html>
