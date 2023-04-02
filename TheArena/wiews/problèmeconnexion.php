cfcf9bc754158051351f1731445e2ab0317acdfd
Lost4295 <turin-ylan@outlook.fr>
2023-04-02 14:05:38 +0200

    creating problèmeconnexion.php

---
+++ TheArena/wiews/problèmeconnexion.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr" data-bs-theme="light">
+<head>
+    <meta charset="UTF-8">
+    <meta name="Ma duper super page" content="Page HTML">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>The Arena-Connexion</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <div class="mx-3 my-2">
+                    <a class="navbar-brand" href="/">
+                        <img src="logothearena-removebg.png" alt="Logo"  class="d-inline-block align-text-center">
+                        <img src="thearenatext-removebg.png" alt="The Arena" class="d-inline-block align-text-center">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="row mr-5 mt-3">
+            <div class="col-6 d-flex flex-column mt-5 justify-content-around align-items-center">
+                <div><h2>Quel est votre problème ?</h2><p>Sélectionnez l’une des options ci-contre afin que nous puissions vous aider à vous connecter.</p></div><div> </div>
+            </div>
+            <div class="col-6">
+                <div class="row">
+                    <div class="col py-2 my-4">
+                        <button class="btn btn-secondary btn-lg py-4 px-4">J'ai oublié mon mot de passe</button>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col py-5 my-4">
+                        <button class="btn btn-secondary btn-lg py-4 px-4">J'ai oublié mon email</button>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col py-5 my-4">
+                        <button class="btn btn-secondary btn-lg py-4 px-4">Mon compte est désactivé</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
