e25e26a27679f307fc9d4c0759d1cc4f4a6b2c1c
Esteban BONNARD <127762924+BonnEsteban@users.noreply.github.com>
2023-04-25 16:55:08 +0200

    test commit

--- TheArena/wiews/admin/users.php
+++ TheArena/wiews/admin/users.php
@@ -41,8 +41,8 @@
 					echo " <td> ". $user [" visibility "]." </td> " ;
 					echo " <td> ". $user [" status "]." </td> " ;
 					echo " <td><a href='core/removeUser.php?id= ". $user [" id "]." ' class='btn btn-danger'>Supprimer</a></td> ";
-
-					echo " </tr> ";
+					
+                    echo " </tr> ";
 				}
 ?>
     </tbody>
