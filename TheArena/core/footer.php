1aaf1d0ac65638e12ddbd49fede711b0b183f395
Esteban BONNARD <127762924+BonnEsteban@users.noreply.github.com>
2023-04-25 18:39:27 +0200

    fix dark mod

--- TheArena/core/footer.php
+++ TheArena/core/footer.php
@@ -3,7 +3,7 @@
 </div>
 <div class="row">
 <div class="col px-0">
-<footer class="footer py-4">
+<footer class="footer py-4 bar">
     <div class="d-flex justify-content-between">
     <img src="../../img/logothearena-removebg.png" alt="Logo"  class="d-inline-block align-text-center logo">
     <div>
