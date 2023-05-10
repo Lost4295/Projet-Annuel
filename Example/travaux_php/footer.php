de07a531acf674f3fbca1ddf798dd4c026ad7760
Lost4295 <turin-ylan@outlook.fr>
2023-05-10 13:47:35 +0200

    remove example site

--- Example/travaux_php/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="footer py-3 my-4 mx-0">
-    <p class="text-center text-muted">&copy; <?php echo date("Y");?> - Ylan Turin--Kondi, Esteban Bonnard, Zacharie Roger</p>
-</footer>
-</div>
-</body>
-</html>
