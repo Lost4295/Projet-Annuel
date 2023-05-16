35c089dd2d3989cf3049763e4b76f973c0d9d7ef
Esteban BONNARD <127762924+BonnEsteban@users.noreply.github.com>
2023-05-16 14:35:27 +0200

    création template email

---
+++ TheArena/core/templateMail/headerMail.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr" >
+<head>
+    <meta charset="UTF-8">
+    <meta name="Ma duper super page" content="Page HTML">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>The Arena</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="/core/css/bootstrap.css">
+    <link rel="stylesheet" href="/core/css/style.css">
+</head>
+<body>
