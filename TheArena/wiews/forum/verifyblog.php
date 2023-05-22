64839a5b713e124ce7bd99205bf57b791a143f42
Lost4295 <turin-ylan@outlook.fr>
2023-05-22 21:56:50 +0200

    fix forums

--- TheArena/wiews/forum/verifyblog.php
+++ TheArena/wiews/forum/verifyblog.php
@@ -59,7 +59,11 @@ if (!$error) {
 } else {
     $_SESSION['blogname']= $blogname;
     $_SESSION['blogdesc']= $blogdesc;
-    $query= $db->prepare("INSERT INTO ".PREFIX."forums (name, description) VALUES (:name,:description)");
-    $query->execute(["name"=>$blogname,"description"=>$blogdesc]);
+    $query = $db->prepare("SELECT id FROM ".PREFIX."users where email =:email");
+    $query->execute(['email'=>$_SESSION['email']]);
+    $result = $query->fetch(PDO::FETCH_ASSOC);
+    $author = $result['id'];
+    $query= $db->prepare("INSERT INTO ".PREFIX."forums (name, description, author) VALUES (:name,:description,:author)");
+    $query->execute(["name"=>$blogname,"description"=>$blogdesc,'author'=>$author]);
     header("Location: /forums");
 }
