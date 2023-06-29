87ccf8bc6e45d1aa6c41f29a3467976f997abacb
Lost4295 <turin-ylan@outlook.fr>
2023-06-29 09:57:53 +0200

    add an automatic mail function on login

--- TheArena/core/sendmail.php
+++ TheArena/core/sendmail.php
@@ -11,6 +11,7 @@ require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
+
 $email = $_SESSION["emailtosend"];
 
 $header = "<!doctype html>
@@ -52,11 +53,11 @@ $bodyla = "<h2 style='width:50%;height:40px;padding-left:120px;text-align:right;
     
     <a style='padding-left:325px; color:orange;' href=" . $urlla . ">Activer votre compte</a>";
 
-    $a = connectToDB();
-    $sql = $a->prepare("SELECT username FROM ".PREFIX."users WHERE email = '$email'");
-    $sql->execute();
-    $result = $sql->fetch(PDO::FETCH_ASSOC);
-    $pseudo = $result["username"];
+$a = connectToDB();
+$sql = $a->prepare("SELECT username FROM " . PREFIX . "users WHERE email = '$email'");
+$sql->execute();
+$result = $sql->fetch(PDO::FETCH_ASSOC);
+$pseudo = $result["username"];
 
 
 $bodyw = "<h2 style='width:50%;height:40px;padding-left:130px;text-align:right;margin:0px;color:#B24909;'>Bienvenue sur The Arena !</h2>
@@ -85,6 +86,123 @@ $bodyfp = "<h2 style='width:50%;height:40px;padding-left:190px;text-align:right;
 <a style='padding-left:325px; color:orange;' href='" . $urlfp . "'>réinitialiser le mot de passe</a>
 <p>Si vous n’êtes pas à l'origine de cette action, veuillez contacter immédiatement notre centre de relation client au <a href='tel:01.23.45.67.89'>01.23.45.67.89</a> (prix d'un appel local) ou par mail à l'adresse suivante : <a href='mailto:postmaster@thearena.litecloud.fr'>postmaster@thearena.litecloud.fr.</a></p>
 ";
+function loginMail($email, $username)
+{
+    $fmt = new IntlDateFormatter('fr_FR', IntlDateFormatter::NONE, IntlDateFormatter::NONE);
+    $fmt->setPattern('EEEE dd MMMM YYYY');
+    $fmt2 = new IntlDateFormatter('fr_FR', IntlDateFormatter::FULL, IntlDateFormatter::FULL);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://api.ip2location.io/?' . http_build_query([
+        'ip'      => $_SERVER['REMOTE_USER'],
+        'key'     => 'C8D8AFF498348B688B2A159636D5B7EE',
+        'format'  => 'json',
+    ]));
+
+    curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
+    $response = json_decode(curl_exec($ch));
+
+    function getBrowser()
+    {
+        $u_agent = $_SERVER['HTTP_USER_AGENT'];
+        $bname = 'Unknown';
+        $platform = 'Plateforme inconnue';
+        $version = "";
+
+        //First get the platform?
+        if (preg_match('/linux/i', $u_agent)) {
+            $platform = 'linux';
+        } elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
+            $platform = 'mac';
+        } elseif (preg_match('/windows|win32/i', $u_agent)) {
+            $platform = 'Windows';
+        } elseif (preg_match('/android/i', $u_agent)) {
+            $platform = 'android';
+        } elseif (preg_match('/iphone/i', $u_agent)) {
+            $platform = 'iphone';
+        } elseif (preg_match('/ipad/i', $u_agent)) {
+            $platform = 'ipad';
+        }
+
+        // Next get the name of the useragent yes seperately and for good reason
+        if (preg_match('/MSIE/i', $u_agent) && !preg_match('/Opera/i', $u_agent)) {
+            $bname = 'Internet Explorer';
+            $ub = "MSIE";
+        } elseif (preg_match('/Firefox/i', $u_agent)) {
+            $bname = 'Mozilla Firefox';
+            $ub = "Firefox";
+        } elseif (preg_match('/OPR/i', $u_agent)) {
+            $bname = 'Opera';
+            $ub = "Opera";
+        } elseif (preg_match('/Chrome/i', $u_agent) && !preg_match('/Edge/i', $u_agent)) {
+            $bname = 'Google Chrome';
+            $ub = "Chrome";
+        } elseif (preg_match('/Safari/i', $u_agent) && !preg_match('/Edge/i', $u_agent)) {
+            $bname = 'Apple Safari';
+            $ub = "Safari";
+        } elseif (preg_match('/Netscape/i', $u_agent)) {
+            $bname = 'Netscape';
+            $ub = "Netscape";
+        } elseif (preg_match('/Edge/i', $u_agent)) {
+            $bname = 'Edge';
+            $ub = "Edge";
+        } elseif (preg_match('/Trident/i', $u_agent)) {
+            $bname = 'Internet Explorer';
+            $ub = "MSIE";
+        }
+
+        // finally get the correct version number
+        $known = array('Version', $ub, 'other');
+        $pattern = '#(?<browser>' . join('|', $known) .
+            ')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
+        if (!preg_match_all($pattern, $u_agent, $matches)) {
+            // we have no matching number just continue
+        }
+        // see how many we have
+        $i = count($matches['browser']);
+        if ($i != 1) {
+            //we will have two since we are not using 'other' argument yet
+            //see if version is before or after the name
+            if (strripos($u_agent, "Version") < strripos($u_agent, $ub)) {
+                $version = $matches['version'][0];
+            } else {
+                $version = $matches['version'][1];
+            }
+        } else {
+            $version = $matches['version'][0];
+        }
+
+        // check if we have a number
+        if ($version == null || $version == "") {
+            $version = "?";
+        }
+
+        return array(
+            'userAgent' => $u_agent,
+            'name'      => $bname,
+            'version'   => $version,
+            'platform'  => $platform,
+            'pattern'    => $pattern
+        );
+    }
+
+    // now try it
+    $ua = getBrowser();
+    $body = "  <h2 style='width:50%;height:40px;padding-left:120px;text-align:right;margin:0px;color:#B24909;'>Nouvelle connexion détectée</h2>
+<p>Bonjour $username,</br>
+Cet e-mail a été créé car une nouvelle connexion au compte $username a été établie le " . $fmt->format($_SERVER['REQUEST_TIME']) . " depuis :<br>
+•	Navigateur : " . $ua['name'] . " " . $ua['version'] . " sur " . $ua['platform'] . "<br>
+•	Lieu :" . $response->city_name . ", " . $response->region_name . ", " . $response->country_name . " (" . $response->zip_code . ")<br>
+•	Adresse IP : " . $_SERVER['REMOTE_ADDR'] . " <br>
+•	Date : " .$fmt2->format($_SERVER['REQUEST_TIME']) . " <br>
+Si vous êtes à l'origine de cette connexion, pas de problème ! Nous voulions juste vérifier qu'il s'agissait de vous.<br>
+Si vous n'êtes pas à l'origine de cette connexion, vous devez immédiatement changer votre mot de passe sur The Arena pour assurer la sécurité de votre compte. <br>
+";
+    sendEmail($email, 'Nouvelle connexion détectée', 45455, $body);
+}
 
 $footer = "<hr />
         <p style='margin-top:8px'>Merci ! <br> L'équipe The Arena</p><div style='text-align:center;'><a href='http://localhost:8000'>The Arena</a></div>
@@ -92,9 +210,7 @@ $footer = "<hr />
         </div>
     </div>
 </body>
-
 </html>";
-
 /**
  * @param string $reciever
  * @param string $subject
@@ -176,6 +292,8 @@ function sendEmail(string $reciever, string $subject, int  $type, string $body =
     if (isset($_SESSION['emailtosend'])) {
         unset($_SESSION["emailtosend"]);
     }
+    echo $rbody;
+    die();
     if (!$mail->send()) {
         echo 'Erreur de Mailer : ' . $mail->ErrorInfo;
     } else {
