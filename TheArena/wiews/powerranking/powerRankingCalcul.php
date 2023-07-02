8677f283717269121c26c111a7d183ca8351a3ca
Esteban BONNARD <127762924+BonnEsteban@users.noreply.github.com>
2023-07-02 11:48:11 +0200

    Update powerRankingCalcul.php

--- TheArena/wiews/powerranking/powerRankingCalcul.php
+++ TheArena/wiews/powerranking/powerRankingCalcul.php
@@ -2,8 +2,8 @@
 require $_SERVER['DOCUMENT_ROOT'] . "/core/functions.php";
 session_start();
 $db=connectToDB();
-$query = $db->prepare("SELECT  t.id , t.score, t.country RANK() OVER (ORDER BY t.score DESC) AS globalRank  FROM (SELECT id, country, MAX(score) AS score 
-    FROM".PREFIX."user WHERE id =200 GROUP BY id, country) AS t");
+$query = $db->prepare("SELECT  ".PREFIX."user.id , ".PREFIX."user.score, ".PREFIX."user.country RANK() OVER (ORDER BY ".PREFIX."user.score DESC) AS globalRank  FROM (SELECT id, country, MAX(score) AS score 
+    FROM".PREFIX."user WHERE id =200 GROUP BY id, country) AS classement");
 
 $query->execute([
                 ]);
